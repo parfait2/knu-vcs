1acf947c37bdd4e10be45cd49e508a333e1212e7
Davin <85967571+dav1n9@users.noreply.github.com>
2023-07-11 12:00:44 +0900

    first commit

---
+++ src/main/java/com/example/knu_vcs/KnuVcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.knu_vcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnuVcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnuVcsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/knu_vcs/KnuVcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knu_vcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuVcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
