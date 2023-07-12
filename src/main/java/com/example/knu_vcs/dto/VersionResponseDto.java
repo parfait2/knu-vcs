77bae0ea9a66c3ca22300828295f8ac76a80ffdb
Davin <85967571+dav1n9@users.noreply.github.com>
2023-07-12 11:09:38 +0900

    Dto 수정

--- src/main/java/com/example/knu_vcs/dto/VersionResponseDto.java
+++ src/main/java/com/example/knu_vcs/dto/VersionResponseDto.java
@@ -6,17 +6,11 @@ import javax.persistence.Column;
 
 public class VersionResponseDto {
 
-    private String os;
     private String ver;
     private int updatetype;
-    private String message;
-    private String packageInfo;
 
     public VersionResponseDto(Version version) {
-        this.os = os;
         this.ver = ver;
         this.updatetype = updatetype;
-        this.message = message;
-        this.packageInfo = packageInfo;
     }
 }
